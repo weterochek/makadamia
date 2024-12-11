d824bd736e72606a55c66c5f9cd7e558350fbfaf
weterochek <132129419+weterochek@users.noreply.github.com>
2024-12-11 09:02:38 +0900

    Update server.js

--- server.js
+++ server.js
@@ -28,10 +28,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
 // Схема и модель пользователя
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
