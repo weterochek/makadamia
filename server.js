2320b15106baa915b37f55ef119c294956c16d6b
weterochek <132129419+weterochek@users.noreply.github.com>
2024-12-11 09:36:30 +0900

    Update server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.post('/login', async (req, res) => {
 });
 
 // Запуск сервера
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
