420d8839dbb8367a96f1833ee29cc35096673b45
weterochek <132129419+weterochek@users.noreply.github.com>
2025-02-26 18:10:19 +0900

    Update server.js

--- server.js
+++ server.js
@@ -54,6 +54,40 @@ app.use((req, res, next) => {
   }
   next();
 });
+const Cart = require("./models/Cart"); // Подключаем модель
+
+app.post('/cart/add', authMiddleware, async (req, res) => {
+  try {
+    const { productId, quantity } = req.body;
+
+    if (!req.user || !req.user.id) {
+      return res.status(401).json({ message: 'Авторизуйтесь, чтобы добавить товар в корзину' });
+    }
+
+    const userId = req.user.id;
+
+    let cart = await Cart.findOne({ userId });
+
+    if (!cart) {
+      cart = new Cart({ userId, items: [] });
+    }
+
+    const existingItem = cart.items.find(item => item.productId.toString() === productId);
+    
+    if (existingItem) {
+      existingItem.quantity += quantity;
+    } else {
+      cart.items.push({ productId, quantity });
+    }
+
+    await cart.save();
+    res.status(200).json({ message: "Товар добавлен в корзину", cart });
+
+  } catch (error) {
+    console.error("Ошибка добавления в корзину:", error);
+    res.status(500).json({ message: "Ошибка сервера" });
+  }
+});
 
 // Указание папки со статическими файлами
 app.use(express.static(path.join(__dirname, "public")));
