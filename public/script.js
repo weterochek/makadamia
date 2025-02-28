3192b1bd601657ac7a85ec8bdd779fe250096150
weterochek <132129419+weterochek@users.noreply.github.com>
2025-02-28 15:38:02 +0900

    Update script.js

--- public/script.js
+++ public/script.js
@@ -17,6 +17,7 @@ window.onload = function() {
     }
   }
 };
+console.log("Отправка запроса на /refresh");
 // Функция для показа/скрытия выпадающего окна корзины под кнопкой "Корзина"
 document.addEventListener("DOMContentLoaded", function() {
     const cartButton = document.getElementById('cartButton');
