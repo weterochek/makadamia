0cf5232bc9c4b886e6a03e20b109ce1a5eba2640
weterochek <132129419+weterochek@users.noreply.github.com>
2025-02-28 15:37:20 +0900

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const allowedOrigins = [
   'https://makadamia.onrender.com',
   'http://localhost:3000', // Для локальной разработки
 ];
-
+console.log("Отправка запроса на /refresh");
 const corsOptions = {
   origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
