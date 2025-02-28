89c9549fce6943f4ce33b4e232aa14da05e3498d
weterochek <132129419+weterochek@users.noreply.github.com>
2025-03-01 00:10:49 +0900

    Update server.js

--- server.js
+++ server.js
@@ -18,13 +18,7 @@ const allowedOrigins = [
 console.log("Отправка запроса на /refresh");
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: true,  // Разрешаем любые источники
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
