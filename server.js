3613510af86cc990a766418101150476f1ec28f2
weterochek <132129419+weterochek@users.noreply.github.com>
2025-03-16 15:56:14 +0900

    Update server.js

--- server.js
+++ server.js
@@ -198,21 +198,19 @@ const User = mongoose.model("User", userSchema);
 // Получение товара по ID
 // Маршрут для получения товара по ID
 app.get('/product/:name', async (req, res) => {
-    const productName = req.params.name;  // Получаем название продукта из параметров URL
+    const productName = req.params.name;  // Получаем название продукта
     try {
-        // Используем модель "Products" для поиска товара по названию
-        const product = await Products.findOne({ name: productName });  // Заменили "findById" на "findOne" и ищем по названию
-
+        const product = await Products.findOne({ name: productName });  // Ищем по имени
         if (!product) {
             return res.status(404).json({ message: 'Товар не найден' });
         }
-
-        res.json(product);  // Отправляем товар в ответ
+        res.json(product);  // Отправляем товар
     } catch (error) {
         console.error('Ошибка при получении товара:', error);
         res.status(500).json({ message: 'Ошибка при получении товара' });
     }
 });
+
 
 
 
