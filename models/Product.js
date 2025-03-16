6b8b5f49736d403874783dfdb78c1e9890fdb054
weterochek <132129419+weterochek@users.noreply.github.com>
2025-03-16 14:11:32 +0900

    Create Product.js

---
+++ models/Product.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  // Добавьте другие поля, если необходимо
+});
+
+module.exports = mongoose.model("Product", productSchema);
