f81985b9ea913f9b5ed9698764d9dd89236404f3
weterochek <132129419+weterochek@users.noreply.github.com>
2025-03-16 19:24:43 +0900

    Update Products.js

--- models/Products.js
+++ models/Products.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    // Можно добавить другие поля, если нужно, например, описание
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  // Добавьте другие поля, если необходимо
 });
 
-module.exports = mongoose.model("Products", productSchema);  // Модель с названием "Products"
+module.exports = mongoose.model("Product", productSchema);
