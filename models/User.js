629ccd4cd68a6b412795d176a591eba7aade10fd
weterochek <132129419+weterochek@users.noreply.github.com>
2025-03-22 02:36:31 +0900

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,7 +1,10 @@
-// В файле models/User.js
 const mongoose = require('mongoose');
+
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    name: { type: String, default: "" },
+    city: { type: String, default: "" }
     orders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }]
 });
 
