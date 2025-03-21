2b1eaad9f5adf3bdf6f139aa28d06521b5291f35
weterochek <132129419+weterochek@users.noreply.github.com>
2025-03-22 02:21:29 +0900

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,8 +2,6 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
     orders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }]
 });
 
