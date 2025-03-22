a03bfe68c60184c0ca8f9e1b39ec8157002d2eb7
weterochek <132129419+weterochek@users.noreply.github.com>
2025-03-22 17:07:00 +0900

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,4 @@
-const jwt = require("jsonwebtoken");
-const JWT_SECRET = process.env.JWT_SECRET || "ai3ohPh3Aiy9eeThoh8caaM9voh5Aezaenai0Fae2Pahsh2Iexu7Qu/";
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     const authHeader = req.headers.authorization;
@@ -7,10 +6,10 @@ module.exports = (req, res, next) => {
         return res.status(401).json({ message: "Токен не предоставлен" });
     }
 
-    const token = authHeader.split(" ")[1];
+    const token = authHeader.split(' ')[1];
     try {
-        const decoded = jwt.verify(token, JWT_SECRET);
-        req.user = decoded;
+        const decoded = jwt.verify(token, process.env.JWT_SECRET || "your_jwt_secret");
+        req.user = decoded;  // Извлекаем данные пользователя
         next();
     } catch (err) {
         return res.status(401).json({ message: "Недействительный токен" });
