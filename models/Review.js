90874eefd7d5b5d1acb7ff2798c79e99bc73aefd
weterochek <132129419+weterochek@users.noreply.github.com>
2025-03-28 20:51:29 +0900

    Update Review.js

--- models/Review.js
+++ models/Review.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
 const reviewSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null }, // Не обязательно required
-    username: { type: String, required: true }, // Тут теперь будет ник + имя
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    username: { type: String, required: true }, // Обязательно сохраняем имя пользователя
     rating: { type: Number, required: true, min: 1, max: 5 },
     comment: { type: String, required: true },
     createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model("Review", reviewSchema);
+
