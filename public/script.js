aaa61c83856229baaf5e2e4031499849802bf2f2
weterochek <132129419+weterochek@users.noreply.github.com>
2025-05-31 16:09:08 +0900

    Update script.js

--- public/script.js
+++ public/script.js
@@ -501,7 +501,7 @@ function displayUserOrders(orders) {
                 <p>Адрес: ${order.address}</p>
                 <p>Дата оформления: ${new Date(order.createdAt).toLocaleDateString()} ${new Date(order.createdAt).toLocaleTimeString()}</p>
                 <p>Время доставки: ${order.deliveryTime || 'Не указано'}</p>
-                <p><strong>Телефон:</strong> ${order.phone || 'не указан'}</p>
+                <p>Телефон: ${order.phone || 'не указан'}</p>
                 <p>Общая сумма: ${order.totalAmount} ₽</p>
                 ${order.additionalInfo ? `<p>Дополнительная информация: ${order.additionalInfo}</p>` : ''}
                 <ul>${itemsList}</ul>
