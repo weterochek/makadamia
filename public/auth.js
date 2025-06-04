e8db05522af6d30697fb22af555837b755db4633
weterochek <132129419+weterochek@users.noreply.github.com>
2025-06-05 03:31:37 +0900

    Update auth.js

--- public/auth.js
+++ public/auth.js
@@ -37,7 +37,7 @@ if (!usernameRegex.test(username)) {
     usernameError.textContent = "";
     usernameError.style.display = "none";
 }
-        const response = await fetch("https://makadamia-app-etvs.onrender.com/register", {
+        const response = await fetch("https://makadamia-e0hb.onrender.com/register", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ username, password, email })
